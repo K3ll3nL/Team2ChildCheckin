0ea7baca87d779f581148532d6ab73c2726ce40e
Will Strickland <wstrickland@smu.edu>
2022-04-04 21:41:40 -0500

    Card template with popup modal completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { ParentPage } from './Parent/parentPage';
 import axios from 'axios';
 
 // React functional component
 function App () {
  
 
-  return (
-    <div>Test</div>
-  );
+  return <>
+    
+  </>;
+  
 }
 
 export default App;
--- frontend/src/Parent/parentCard.jsx
+++ frontend/src/Parent/parentCard.jsx
@@ -1,7 +1,75 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 export const ParentCard = () => {
+    const [open, setOpen] = React.useState(false);
 
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     
     return <>
-        
+         <Card variant="outlined"sx={{ minWidth: 275},{m:5}}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Wyatt Saltzman
+        </Typography>
+
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Current Room: Room 1
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Chaperone: John Doe
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Health Status: Healthy
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Recent Behavioral Concerns: None
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => {
+            handleClickOpen();
+            console.log('edit child form')
+        }}
+        size="small">Edit this childs needs</Button>
+        <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Child Information</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Use this form to add any necessary notes about your child.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Note"
+            type="Note"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Add Note</Button>
+        </DialogActions>
+      </Dialog>
+      </CardActions>
+    </Card>
     </>;
 }
