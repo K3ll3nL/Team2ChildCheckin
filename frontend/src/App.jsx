6d03024148a91390896dceca5fa1db6e360ad907
Will Strickland <wstrickland@smu.edu>
2022-04-05 19:40:03 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { ParentPage } from './Parent/parentPage';
+import  ResponsiveAppBar from './NavBar';
 import axios from 'axios';
 
 // React functional component
 function App () {
  
+
   return <>
     {/* To look at example parent page comment/uncomment following line */}
+    <ResponsiveAppBar/>
     <ParentPage/>
   </>;
   
