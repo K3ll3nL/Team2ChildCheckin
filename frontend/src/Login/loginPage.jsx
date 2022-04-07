17cc8ee0b32b8ed82723d659d9566b56af3551b3
Alexander Shockley <60485310+AlexShock66@users.noreply.github.com>
2022-04-07 11:58:21 -0500

    Login Page visuals mostly Done
    resolves #5

--- frontend/src/Login/loginPage.jsx
+++ frontend/src/Login/loginPage.jsx
@@ -1,13 +1,13 @@
-import {  Button, Grid } from '@mui/material'
+import { Button, Card, CardActions, CardContent, CardHeader, Grid, Link, Typography } from '@mui/material'
 import TextField from '@mui/material/TextField'
 
 import '../index.css'
 import { useState } from 'react'
 import { Box } from '@mui/system'
 export const LoginPage = () => {
-    const [username,setUsername] = useState('');
-    const [password,setPassword] = useState('');
-    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleUserChange = (e) => {
         setUsername(e.target.value);
     }
@@ -17,18 +17,32 @@ export const LoginPage = () => {
 
     const handleButtonClick = () => {
         //TODO Send information to api and see if it is a valid name
+        alert(`Username: ${username}\nPassword: ${password}`);
     }
+
+
     console.log(username);
     return (
-        
+
         <Box >
-            <h1>Login</h1>
-            <Grid container justifyContent="center">
-                <form>
-                    <TextField id="username" label="Username" variant="outlined" onChange={handleUserChange}/>
-                    <TextField id="password" label="Password" variant="outlined" type="password" onChange={handlePassChange}/>
-                    <Button variant="contained">Login</Button>
-                </form>
+
+            <Grid container justifyContent="center" >
+                <Card>
+                    <CardHeader title="Login"></CardHeader>
+                    <CardContent>
+
+                        <form>
+                            <TextField id="username" label="Username" variant="outlined" onChange={handleUserChange} margin="normal" />
+                            <TextField id="password" label="Password" className="" variant="outlined" type="password" margin="normal" sx={{ display: "block" }} onChange={handlePassChange} />
+                        </form>
+                        <Typography variant="subtitle2" sx={{marginTop: 3, display: "inline"}}>Need an account? </Typography>
+                        <Link href="https://google.com">SIGN UP</Link>
+                    </CardContent>
+                    <CardActions>
+                        <Button variant="contained" fullWidth margin="normal" onClick={handleButtonClick}>Login</Button>
+
+                    </CardActions>
+                </Card>
             </Grid>
         </Box>
 
