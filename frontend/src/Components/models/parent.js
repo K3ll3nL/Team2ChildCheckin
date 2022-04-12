0727e0ec9d2c3daa02a247b752e6f0538af86b8e
wyattsaltzman <54828648+wyattsaltzman@users.noreply.github.com>
2022-04-11 20:41:18 -0500

    started employee dashboard
    lists of parents

---
+++ frontend/src/Components/models/parent.js
@@ -0,0 +1,9 @@
+export class Parent {
+    constructor(id, name, phone, email, children) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.children = children;
+    }
+}
---
+++ frontend/src/Components/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material';
+export const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#819ca9',
+            main: '#546e7a',
+            dark: '#29434e',
+            contrastText: '#ffffff',
+        },
+        secondary: {
+            light: '#fffffb',
+            main: '#d7ccc8',
+            dark: '#a69b97',
+            contrastText: '#000000',
+        },
+    },
+});
