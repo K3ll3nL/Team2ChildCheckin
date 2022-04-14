7cf9639934e299ed7a0f3e17294a14cc87cc542d
Will Strickland <wstrickland@smu.edu>
2022-04-14 12:14:59 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,10 @@ import './App.css';
 import ParentPage from './Components/Parent/parentPage';
 import axios from 'axios';
 
-import { LoginPage } from './Login/loginPage';
 import { requirePropFactory } from '@mui/material';
-import { RegistrationPage } from './Login/registration';
+
 import ResponsiveAppBar from './Components/NavBar';
-import { EmployeeCard } from './Components/models/Employee/EmployeeCard';
+// import { EmployeeCard } from './Components/models/Employee/EmployeeCard';
 
 
 // React functional component
@@ -17,8 +16,8 @@ function App () {
 
   return (
     <>
-    <ResponsiveAppBar />
-   <EmployeeCard />
+
+    
    </>
   );
 
